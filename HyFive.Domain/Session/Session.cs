b30b69c66cdf4f29d619b869f47af299f62b839d
George Katsis <Georgios.Katsis@ext.ecdc.europa.eu>
2025-05-28 16:13:53 +0300

    fix session data

--- HyFive.Domain/Session/Session.cs
+++ HyFive.Domain/Session/Session.cs
@@ -7,9 +7,9 @@ namespace HyFive.Domain.Session
 
     public class Session
     {
-        public DateTime CreatedDate { get; set; }
-        public DateTime StartDate { get; set; }
         public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime StartDate { get; set; }  
         public Department Department { get; set; }
         public Observer Observer { get; set; }
         public string Comment { get; set; }
