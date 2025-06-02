0f49af3587b7f8f06158955dcb2c2e2b5039c8ed
ikaramitsos <ioannis.karamitsos@ext.ecdc.europa.eu>
2025-06-02 16:23:12 +0300

    admin ui - remove app-search-hprnumber-link component + reomve fhiAdmin texts + fix tab labels

--- HyFive.Admin/ClientApp/src/app/fhiadmin/oversikt-fhiadmin/edit-fhiadmin/edit-fhiadmin.component.ts
+++ HyFive.Admin/ClientApp/src/app/fhiadmin/oversikt-fhiadmin/edit-fhiadmin/edit-fhiadmin.component.ts
@@ -35,7 +35,7 @@ export class EditFhiAdminComponent implements OnInit, OnDestroy {
   loadFhiAdmin() {
     this.userService.getFhiAdmin().subscribe(
       (fhiAdmins) => this.users = fhiAdmins,
-      (error) => this.toastrService.error('An error occurred while loading FhiAdmin: ' + error?.message, '', { disableTimeOut: true}),
+      (error) => this.toastrService.error('An error occurred while loading Admin: ' + error?.message, '', { disableTimeOut: true}),
     );
   }
 
@@ -50,8 +50,8 @@ export class EditFhiAdminComponent implements OnInit, OnDestroy {
 
   createFhiAdmin() {
     this.userService.createFhiAdmin(this.newFhiAdmin).subscribe(
-      () => this.toastrService.success('FhiAdmin create'),
-      error => this.toastrService.error('An error occurred while creating FhiAdmin: ' + error?.error, '', { disableTimeOut: true}),
+      () => this.toastrService.success('Admin create'),
+      error => this.toastrService.error('An error occurred while creating Admin: ' + error?.error, '', { disableTimeOut: true}),
       () => { this.newFhiAdmin = null; this.loadFhiAdmin(); }
     );
   }
@@ -65,10 +65,10 @@ export class EditFhiAdminComponent implements OnInit, OnDestroy {
   updateFhiAdmin(fhiAdmin: User) {
     this.userService.updateFhiAdmin(fhiAdmin).subscribe(
       (updatedUser) => {
-        this.toastrService.success('FhiAdmin updated');
+        this.toastrService.success('Admin updated');
         this.loadFhiAdmin();
       },
-      error => this.toastrService.error('An error occurred while updating FhiAdmin: ' + error?.error, '', { disableTimeOut: true}),
+      error => this.toastrService.error('An error occurred while updating Admin: ' + error?.error, '', { disableTimeOut: true}),
       () => this.fhiAdminAsChanged = null
     );
   }
