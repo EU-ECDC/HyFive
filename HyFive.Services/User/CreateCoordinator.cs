b8124fb0b0b388b4e515a3032c81ad8ab0e7de78
George Katsis <Georgios.Katsis@ext.ecdc.europa.eu>
2025-06-10 14:09:59 +0300

    remove validations on create observer

--- HyFive.Services/User/CreateCoordinator.cs
+++ HyFive.Services/User/CreateCoordinator.cs
@@ -36,11 +36,11 @@ namespace HyFive.Services.User
                     throw new Exception("Could not find institution with ID: " + command.USer.InstitutionId);
                 }
 
-                //if (!UserValidator.HasNameAndHprNumberOrValidPseudonym(command.USer))
-                //{
-                //    throw new ArgumentException("Coordinator must have first name, last name, and either HPR number or pseudonym");
-                //}
-                
+                if (!UserValidator.HasNameAndHprNumberOrValidPseudonym(command.USer))
+                {
+                    throw new ArgumentException("Coordinator must have first name, last name, and either HPR number or pseudonym");
+                }
+
                 var coordinator = new Coordinator()
                 {
                     FirstName = command.USer.FirstName,
