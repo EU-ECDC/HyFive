5382a1a4c9c80401c85e4fd23c34dcb1ed91ea07
ikaramitsos <ioannis.karamitsos@ext.ecdc.europa.eu>
2025-06-16 13:57:52 +0300

    ui fixes admin module - institutions tab, coordinator, not accepting special char on firstname, lastname input fields

--- HyFive.Admin/ClientApp/src/app/_common/edit-coordinators/editCoordinators.component.ts
+++ HyFive.Admin/ClientApp/src/app/_common/edit-coordinators/editCoordinators.component.ts
@@ -144,6 +144,12 @@ export class EditCoordinatorsComponent implements OnInit, OnDestroy {
     }
   }
 
+    omitSpecialChar(event) {   
+    var k;  
+    k = event.charCode;  //         k = event.keyCode;  (Both can be used)
+    return((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57)); 
+  }
+
   identityPseudonymChanged(coordinator: User, identityPseudonym: string) {
     coordinator.identityPseudonym = identityPseudonym;
   }
