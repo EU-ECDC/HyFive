ed8bfc0d73c618b7a558f8bff49ada0ac66b629e
George Katsis <Georgios.Katsis@ext.ecdc.europa.eu>
2025-07-01 14:29:35 +0300

    enable Five Indication compliance reports

--- HyFive.Admin/ClientApp/src/app/coordinator/rapporter/report.component.ts
+++ HyFive.Admin/ClientApp/src/app/coordinator/rapporter/report.component.ts
@@ -19,15 +19,15 @@ export class ReportComponent implements OnInit {
 
   private getTreeviewNavigationItems(): FhiTreeViewNavigationItem[] {
     return [
-      // {
-      //   name: 'five indications',
-      //   children: [
-      //     {
-      //       name: 'Etterlevelse',
-      //       routerLink: UrlPaths.fireindikasjonerEtterlevelse
-      //     }
-      //   ]
-      // },
+      {
+        name: 'five indications',
+        children: [
+          {
+            name: 'Etterlevelse',
+            routerLink: UrlPaths.fiveIndicationsCompliance
+          }
+        ]
+      },
       {
         name: 'Predifined (PDF)',
         isExpanded: true,
