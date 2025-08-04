a63e555195a05fe85c4d35f08a0d9b6aa3c192fb
ikaramitsos <ioannis.karamitsos@ext.ecdc.europa.eu>
2025-08-04 14:58:27 +0300

    ui changes - institutionsName to institutionName in both apps (plus admin module)

--- HyFive.Admin/ClientApp/src/app/models/api/Session.ts
+++ HyFive.Admin/ClientApp/src/app/models/api/Session.ts
@@ -10,7 +10,7 @@ export interface Session<TObservation>
 	department: Department;
 	createdDate: Date;
 	observations?: TObservation[];
-	institutionName?: string;
+	institutionsName?: string;
 	institutionId?: number;
 	comment?: string;
 }
