2f81c2468a33edf21d9890ee57114bfde916b953
George Katsis <Georgios.Katsis@ext.ecdc.europa.eu>
2025-08-08 11:26:43 +0300

    make email, first name and last name mandatory in user when create or update him

--- HyFive.Services/User/CreateCoordinator.cs
+++ HyFive.Services/User/CreateCoordinator.cs
@@ -36,9 +36,9 @@ namespace HyFive.Services.User
                     throw new Exception("Did not find institution with ID: " + command.User.InstitutionId);
                 }
 
-                if (!UserValidator.HasNameAndHprNumberOrValidPseudonym(command.User))
+                if (!UserValidator.HasNameAndEmail(command.User))
                 {
-                    throw new ArgumentException("Coordinator must have first name, last name, and either HPR number or pseudonym");
+                    throw new ArgumentException("Coordinator must have first name, last name, and email");
                 }
 
                 var coordinator = new Coordinator()
