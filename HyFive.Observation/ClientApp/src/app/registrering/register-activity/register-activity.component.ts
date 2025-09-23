2a25885e134c2ed93551de16fb9bf6e59043c722
ioanniska <ioannis.karamitsos@ext.ecdc.europa.eu>
2025-09-23 15:43:35 +0300

    ui observer module - change text from 'Timekeeping' to 'Time' on Observation page + change variable name from 'timekeeping' to 'time' on whole oberver module + 'Protective equipment' to PPE

--- HyFive.Observation/ClientApp/src/app/registrering/register-activity/register-activity.component.ts
+++ HyFive.Observation/ClientApp/src/app/registrering/register-activity/register-activity.component.ts
@@ -23,7 +23,7 @@ export class RegisterActivityComponent implements OnInit {
   usedGloves?: boolean = null;
 
   @Input("parentId") parentId: string;
-  @Input("timekeeping") timekeeping: boolean;
+  @Input("time") time: boolean;
   @Input("disabled") disabled: boolean;
   @Input("activityType") activityType: ActivityType;
   @Input("seconds") seconds: number;
@@ -63,7 +63,7 @@ export class RegisterActivityComponent implements OnInit {
       this.modalService.open(modalName, { windowClass: 'hh-modal' });
     }
     else {
-      if (this.timekeeping) {
+      if (this.time) {
         this.startOrStopTimer();
         this.showText = false;
       }
@@ -77,7 +77,7 @@ export class RegisterActivityComponent implements OnInit {
 
     this.usedGloves = wasConfirmed;
 
-    if (this.timekeeping) {
+    if (this.time) {
       this.startOrStopTimer();
       this.showText = false;
     }
