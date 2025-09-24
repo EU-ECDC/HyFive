1ff8eefdd9db8cc1de7d3d818934d36d7e538412
ioanniska <ioannis.karamitsos@ext.ecdc.europa.eu>
2025-09-24 17:15:35 +0300

    ui observer module - make distinct and sort the list of healthcare facilities for dropdown 'Healthcare Facilities'

--- HyFive.Observation/ClientApp/src/app/services/data/FacilityService.ts
+++ HyFive.Observation/ClientApp/src/app/services/data/FacilityService.ts
@@ -24,7 +24,11 @@ export class FacilityService {
 
   getFacilities(): Observable<Facility[]> {
     const url = `${environment.apiBaseUrl}/v1/facility/`;
-    return this.http.get<Facility[]>(url).pipe(map(data => data.filter(x => x != null)));
+    return this.http.get<Facility[]>(url).pipe(
+                                                map(data => data.filter(x => x != null)),
+                                                map(data => [...new Map(data.map(item => [item.id, item])).values()]
+                                                            .sort((a,b) => a.name.localeCompare(b.name, undefined, { sensitivity: "base" })))
+                                              );
   }
 
   getSelectedFacilityId(): number | null {
