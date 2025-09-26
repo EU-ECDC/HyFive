ea3b5a0bf33ca6ecfefc070d663aab83d61ac1c6
Georgios Katsis <Georgios.Katsis@ext.ecdc.europa.eu>
2025-09-26 13:44:51 +0000

    Merged PR 14657: resolve the bug where you cannot edit the city
    ### 📝 Summary
    
    _Replace this text and provide a high-level summary of the changes and optionally add notes for the reviewer._
    
    ### ✅ Pre-merge Checks
    
    - [ ] Related work items are linked (User Stories, Defects, etc.) to the Pull Request
    - [ ] Documentation updated _(if applicable)_
    - [ ] Release notes updated in Product Release work item _(if applicable)_
    - [ ] Tests added/updated _(if applicable)_
    - [ ] Code follows the [ECDC Development Best Practices](https://dev.azure.com/EU-ECDC/DevOps%20Guidance/_wiki/wikis/ECDC%20DevOps%20Guidance/3249/Development-Best-Practices)
    
    ### ✏️ Commit Message(s)
    
    resolve the bug where you cannot edit the city
    
    Related work items: #118391

--- HyFive.Services/Facility/UpdateFacility.cs
+++ HyFive.Services/Facility/UpdateFacility.cs
@@ -36,6 +36,12 @@ namespace HyFive.Services.Facility
                     throw new ArgumentException(
                         $"Facility type with ID {command.Facility.FacilityType.Id} was not found in the database.");
 
+                //check city:
+                var city = await _context.City.FirstOrDefaultAsync(i => i.Id == command.Facility.City.Id);
+                if (city == null)
+                    throw new ArgumentException(
+                        $"City with ID {command.Facility.City.Id} was not found in the database.");
+
                 var facility = await _context.Facility
                                                 .Include(i => i.City)
                                                 .FirstOrDefaultAsync(i => i.Id == command.Facility.Id);
@@ -44,6 +50,7 @@ namespace HyFive.Services.Facility
                 facility.Abbreviation = command.Facility.Abbreviation;
                 facility.HERId = command.Facility.HERId;
                 facility.FacilityType = facilityType;
+                facility.City = city;
 
                 _context.Facility.Update(facility);
                 await _context.SaveChangesAsync(cancellationToken);
